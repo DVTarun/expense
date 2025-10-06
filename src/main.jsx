3968551b60f2f9eacf222eba6ec7461be2d4172a
DVTarun <163630036+DVTarun@users.noreply.github.com>
2025-10-06 10:52:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,33 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./App";
-
 import Login from "./pages/Login";
-
 import Register from "./pages/Register";
-
 import Forgot from "./pages/Forget";
-
-
-import Dashboard from "./dashboard";
-
-import ResetPassword from "./pages/Resetpassword";
+import Dashboard from "./Dashboard.jsx";
+import ResetPassword from "./pages/Resetpassword.jsx";
 
 import Expense from "./DashPage/Expense";
-
 import Transaction from "./DashPage/Transaction";
 import SetBudget from "./DashPage/SetBudget";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
   <React.StrictMode>
-
-    <Router>
-
+    {/* 👇 Add basename so all routes work under /myexpense  basename="/myexpense"*/}
+    <Router basename="/myexpense" >
       <Routes>
-
         <Route path="/" element={<App />} />
-
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
-        <Route path="/forget" element={<Forgot/>}/>
-
-        <Route path="/dashboard" element={<Dashboard/>}/>
-
+        <Route path="/forget" element={<Forgot />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-
-        <Route path="/expense" element={<Expense/>}/>
-
-        <Route path="/transaction" element={<Transaction/>}/>
-
-        <Route path="/setbudget" element={<SetBudget/>}/>
+        <Route path="/expense" element={<Expense />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/setbudget" element={<SetBudget />} />
       </Routes>
-
     </Router>
-
   </React.StrictMode>
-
 );
